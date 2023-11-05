4fcdc68b2e745844b8596151c9c45a9d7a875acf
prajaktapatil-18 <prajupatil0327@gmail.com>
2023-11-05 00:26:31 -0400

    Low-level-que

---
+++ src/DaysOfWeek.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class DaysOfWeek {
+public static void main(String []args){
+
+    Scanner scan =   new Scanner(System.in);
+
+    int choice = scan.nextInt();
+    switch (choice){
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("thursday");
+            break;
+        case 5:
+            System.out.println("friday");
+            break;
+        case 6:
+
+            System.out.println("Saturday");
+
+            break;
+        case 7:
+            System.out.println("Sunday");
+            break;
+        default:
+            System.out.println("invalid number");
+
+    }
+
+
+
+
+}
+
+
+
+
+       /*
+       n =4
+       4+3+2+1
+
+
+       n+n-1
+        */
+
+        }
+
+
