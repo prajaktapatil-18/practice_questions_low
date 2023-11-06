ec84fdb4895e57c63ef5c219b2fa39d0ae90535a
prajaktapatil-18 <prajupatil0327@gmail.com>
2023-11-05 23:58:47 -0500

    Harries quiz

---
+++ src/HarryMeghan.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HarryMeghan {
+    public static void main(String[]args){
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("enter that number which given by meghan");
+        int num = scan.nextInt();
+
+        System.out.println((num*3)+7-10);
+    }
+}
