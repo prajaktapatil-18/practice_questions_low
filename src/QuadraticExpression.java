7385e1301ace52105b8c1e2125f2a5aa124d8755
prajaktapatil-18 <prajupatil0327@gmail.com>
2023-11-06 00:16:09 -0500

    Quadratic Expression

---
+++ src/QuadraticExpression.java
@@ -0,0 +1,14 @@
+
+         import java.util.Scanner;
+    public class QuadraticExpression{
+        public static void main(String[]args){
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter your number");
+            int num = scan.nextInt();
+
+            System.out.println((num*num)+(2*num)+1);
+
+        }
+    }
+
